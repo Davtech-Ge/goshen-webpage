3f0759604b3bbd9ed107cb93abc351b5c7c5ed4f
Davtech-Ge <Davidgeorgechigoziri@gmail.com>
2025-05-06 12:45:17 +0100

    refactor: adjust positioning of AboutUS section content for improved layout

--- frontend/src/components/AboutUS.js
+++ frontend/src/components/AboutUS.js
@@ -5,7 +5,7 @@ const AboutUS = () => {
     <section className='bg-black text-white py-6 px-4'>
         <div className=' relative w-full mx-auto flex flex-col md:flex-row items-center'>
             <img src='/images/aboutGosh.jpg' alt='Apostle glory helen'  className='rounded-lg w-full h-1/2 lg:h-screen mb-6 md:mb-0 md:mr-8' />
-            <div className='absolute'>
+            <div className='absolute bottom-0'>
                 <h2 className='text-2xl font-semibold mb-4'>Christ Goshen city Assembly</h2>
                 <p className='text-gray-300 mb-4'>
                 Christ Goshen is a weekly Apostolic and prophetic platform where people come to experience the Word, Worship, Miracle and Love. The revelatory teachings help believers to experience true intimacy with the Holy Spirit and learn to be with Him, be like Him and Represent Him
