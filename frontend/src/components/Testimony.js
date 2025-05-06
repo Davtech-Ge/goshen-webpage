d6af3550d810a3b06e678de2a8ec0170c445bea4
Davtech-Ge <Davidgeorgechigoziri@gmail.com>
2025-05-06 13:40:59 +0100

    refactor: update Testimony component layout and styling for improved readability and design

--- frontend/src/components/Testimony.js
+++ frontend/src/components/Testimony.js
@@ -5,15 +5,15 @@ const Testimony = () => {
     <div className='w-full h-4/5 bg-black p-4'>
         <div className='relative '>
         <img src='/images/externalminis.jpg' alt='testimonies' className='w-full h-45 object-cover relative' />
-               <div className='absolute top-0 left-0 p-4'>
-               <h1>TESTIMONIES</h1>
-               <h1>(FAITH STORIES)</h1>
-               </div>
+               
+              
 
                <div className='absolute bottom-0 right-0 p-4'>
-               <p>We welcome testimonies from our online viewers connecting across the world. please share your testimonies with us via:</p>
-                <p>christgoshencitybethel@gmail.com</p>
-                <button className='border-yellow-400 rounded-full text-yellow-400 hover:text-yellow-500'>SEND TESTIMONY</button>
+               <h1 className='text-sm text-gray-400'>TESTIMONIES</h1>
+               <h1 className='text-sm text-gray-400'>(FAITH STORIES)</h1>
+               <p className='text-xs text-gray-400'>We welcome testimonies from our online viewers connecting across the world. please share your testimonies with us via:</p>
+                <p className='text-xs text-blue-800 underline'>christgoshencitybethel@gmail.com</p>
+                <button className='border border-yellow-400 rounded-full text-sm p-2 text-yellow-400 hover:text-yellow-500'>SEND TESTIMONY</button>
                </div>
         </div>
     </div>
